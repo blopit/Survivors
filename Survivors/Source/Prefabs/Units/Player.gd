ac68bdc870c5f39b513befc70d2ef9c70e332019
Shren Patel <patel.shrenil@gmail.com>
2024-02-08 22:17:55 -0500

    temporary friction

--- Survivors/Source/Prefabs/Units/Player.gd
+++ Survivors/Source/Prefabs/Units/Player.gd
@@ -15,10 +15,16 @@ func _process(delta):
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
-	
+
 	#velocity = velocity.linear_interpolate(Vector2.ZERO, friction * delta)
 	#move_and_collide(velocity)
 	apply_force(velocity)
+	
+	var speed = linear_velocity.length()
+	if speed > friction:
+		apply_force(linear_velocity.normalized() * -friction)
+	else:
+		apply_force(linear_velocity.normalized() * -speed)
 	
 	
 
